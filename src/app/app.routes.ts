14f44974d0c5c38b156c5370d5c0ae271d6cf3b3
Pedro Araya Gálvez <[piteraraya@gmail.com]>
2020-11-09 04:43:44 -0300

    usehash en routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,4 +16,4 @@ const APP_ROUTES: Routes = [
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
